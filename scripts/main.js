e56ba11df0f25ada3019a8be2a76d13b54035626
Rafael Cotta <rcotta@gmail.com>
2025-11-10 23:38:44 -0300

    versão intermediaria

--- scripts/main.js
+++ scripts/main.js
@@ -21,10 +21,49 @@ C. Troca primeira partida
     - recalcular tabela
 
 
+
+[events, html controls]
+REFRESH_HTML_CONTROLS -> onChange dos controles HTML
+
+[modelo]
+getValidGames
+
+
+[servidor remoto]
+loadPlayerData(playerUserName) -> carrega dados de URLs ou do localStorage (cache)
+parseRawGameData(rawData, currentUserName) -> helper para "limpar" dados antes de guardar no cache
+
+[cache]
+putLocalStorage / getLocalStorage -> abstração para local storage
+putSessionStorage / getSessionStorage -> abstração para session storage
+
+
+
 */
 
+const DATE_TIME_FMT = new Intl.DateTimeFormat(navigator.language, {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit'
+});
 
 
+USER_NAME = "ctrl_player"
+TIME_CLASS = "ctrl_time_class"
+BASE_MONTH = "ctrl_base_month"
+BASE_MATCH = "ctrl_base_match"
+
+TIME_CONTROLS = {
+    "180+1": "Blitz 3 min + 1",
+    "180": "Blitz 3 min",
+    "300": "Blitz 5 minutos",
+    "600": "Rápida 10 minutos",
+    "60": "Bullet 1 minuto"
+}
+
+// tipos de empate
 DRAW_STR = new Set(["repetition", "agreed", "insufficient", "timevsinsufficient"])
 
 
@@ -44,11 +83,11 @@ function cacheKey(key) {
     return "_cache|" + key
 }
 
-function getSession(key) {
+function getSessionStorage(key) {
     return sessionStorage[cacheKey(key)] ? JSON.parse(sessionStorage[cacheKey(key)]) : undefined
 }
 
-function putSession(key, value) {
+function putSessionStorage(key, value) {
     try {
         sessionStorage[cacheKey(key)] = JSON.stringify(value)
         return value
@@ -57,11 +96,11 @@ function putSession(key, value) {
     }
 }
 
-function getLocalCache(key) {
+function getLocalStorage(key) {
     return localStorage[cacheKey(key)] ? JSON.parse(localStorage[cacheKey(key)]) : undefined
 }
 
-function putLocalCache(key, value) {
+function putLocalStorage(key, value) {
     try {
         localStorage[cacheKey(key)] = JSON.stringify(value)
         return value
@@ -78,7 +117,13 @@ function parseRawGameData(rawData, currentUserName) {
         return e.rated
     })
 
+    f= false
+
     games.forEach(e => {
+        if (!f) {
+            console.log(e)
+            f=true
+        }
         v = {}
         v.end_time = e.end_time
         v.uuid = e.uuid        
@@ -111,6 +156,7 @@ async function loadPlayerData(playerUserName) {
     while (p_archives["archives"].length) { 
 
         let url = p_archives["archives"].pop()
+        let gameDataCacheKey = "games://" + url
 
         // FIXME: remover
         if (url.indexOf("/2025") < 0) continue
@@ -119,14 +165,14 @@ async function loadPlayerData(playerUserName) {
 
         if (url <= six_months_ago_url) {
             // buscar no cache
-            gamesData = getLocalCache(url)
+            gamesData = getLocalStorage(gameDataCacheKey)
             if (!gamesData) {
                 console.log("## não cacheado [1]: " + url)
                 rawData = await getJson(url)
-                gamesData = putLocalCache(url, parseRawGameData(rawData, playerUserName))
+                gamesData = putLocalStorage(gameDataCacheKey, parseRawGameData(rawData, playerUserName))
             }
         } else {
-            console.log("## não cacheado [2]: " + url)
+            console.log("## não cacheado [url recente]: " + url)
             gamesData = parseRawGameData(await getJson(url), playerUserName)
         }
 
@@ -139,13 +185,11 @@ async function loadPlayerData(playerUserName) {
 
 }
 
-// função que faz refresh da lista de jogos
-async function refreshModel() {
+// refreshes playeName's game list and save on sessionStorage
+async function refreshModel(playerName) {
 
-    player = currentPlayer()
-    allGames = await loadPlayerData(player)
-    sessionStorage["games"] = JSON.stringify(allGames)
-    
+    allGames = await loadPlayerData(playerName)
+    putSessionStorage("games", allGames)
     console.log("Atualizado modelo com " + allGames.length + " partidas")
 
 }
@@ -166,45 +210,18 @@ function saveForm(player) {
 
 
 
-function ctrl_player_OnChange() {
 
-    refreshModel()
 
-    key = "form|" + player
-    data = getLocalCache(key)
-
-    if (data) {
-        if (data.ctrl_time_class) {
-            $("ctrl_time_class").val(data.ctrl_time_class)
-        }
-
-        /*
-        if (data.ctrl_base_match) {
-            $("ctrl_base_match").val(data.ctrl_base_match)
-        }
-        */
-
-    }
+function clearAll() {
+    localStorage.clear()
+    sessionStorage.clear()
+    console.info("Tudo limpo!")
 }
 
 
 
 
 
-
-
-BASE_MONTH = "baseMonth"
-USER_NAME = "username"
-TIME_CLASS = "timeClass"
-BASE_MATCH = "baseMatch"
-
-TIME_CONTROLS = {
-    "180+1": "Blitz 3 min + 1",
-    "180": "Blitz 3 min",
-    "300": "Blitz 5 minutos",
-    "600": "Rápida 10 minutos",
-    "60": "Bullet 1 minuto"
-}
 
 
 matches = []
@@ -251,30 +268,139 @@ function loadMatches() {
     
 }
 
+async function getValidGames(playerName, timeClass, baseStartTime) {
+    console.log(`getValidGames(${playerName}, ${timeClass}, ${baseStartTime})`)
+    await refreshModel(playerName)
+    allGames = getSessionStorage("games")
+    console.log(`Recuperado do sessionStorage ${allGames.length} jogos`)
+    validGames = allGames.filter(e => e.end_time >= baseStartTime && e.time_class == timeClass)
+    return validGames
+}
+
+REFRESH_HTML_CONTROLS = async function(e) {
+
+    /*
+    ctrl_player
+    ctrl_time_class
+    ctrl_base_month
+    ctrl_base_match
+    */
+
+    // id e valor do controle acionado
+    srcId = (e.currentTarget.id)
+    srcValue = (e.currentTarget.value)
+
+    level = [USER_NAME, TIME_CLASS, BASE_MONTH, BASE_MATCH].indexOf(srcId)
+
+    console.log("Alterando controle " + srcId + " para valor " + srcValue + " (level:" + level + ")")
+
+    if (level < 0) {
+        console.error("Controle " + srcId + " não identificado!")
+        return
+    }
+
+    playerName = $("#" + USER_NAME).val()    
+    timeClass = $("#" + TIME_CLASS).val()    
+    baseMonth = $("#" + BASE_MONTH).val()    
+    baseGame = $("#" + BASE_MATCH).val()    
+
+    if (!playerName) {
+        console.info("Nenhum usuário selecionado!")
+        return
+    }
+
+    $("#loading-indicator").show()    
+
+    // player
+    if (level == 0) {
+        
+        console.log("Limpar partida de referência")     
+        $("#" + BASE_MATCH + " option:selected").prop('selected', false);   
+        
+        console.log(`--> Carregar partidas de ${playerName}`)
+
+        validGames = await getValidGames(playerName, timeClass, baseMonth)
+        options = {}
+        validGames.forEach(game => {
+            sDt = DATE_TIME_FMT.format(game.end_time * 1000)
+            options[game.uuid] = `${sDt} - ${game.opponent} (${game.rating}, ${game.result == 1 ? 'win' : (game.result == 0 ? 'loss' : 'draw')})`
+        })
+        setSelectOptions($(`#${BASE_MATCH}`), options)
+
+
+
+        console.log(`--> ${validGames.length} partidas válidas!`)
+
+        console.log("Carregar partidas filtradas por mês base e tipo de controle de tempo em partida de referência")
+        console.log("se P=playerName/sessionStorage/<tipo partida>/<mes base>, e partida referencia existe, selecionar partida de referência P")
+
+        
+    }
+
+    // tipo de partida
+    if (level == 1) {
+        console.log("Limpar partida de referência")        
+        console.log("Carregar partidas de " + playerName)
+        console.log("Carregar partidas filtradas por mês base e tipo de controle de tempo em partida de referência")
+        console.log("se P=playerName/sessionStorage/<tipo partida>/<mes base>, e partida referencia existe, selecionar partida de referência P")
+    }
+
+    // mês base
+    if (level == 2) {
+        console.log("Limpar partida de referência")        
+        console.log("Carregar partidas de " + playerName)
+        console.log("Carregar partidas filtradas por mês base e tipo de controle de tempo em partida de referência")
+        console.log("se P=playerName/sessionStorage/<tipo partida>/<mes base>, e partida referencia existe, selecionar partida de referência P")
+    }
+
+
+    if (level == 3) {
+        console.log("salvar P=playerName/sessionStorage/<tipo partida>/<mes base>")
+    }
+
+
+    $("#loading-indicator").hide()
+
+
+}
+
+function setSelectOptions(ctrl, options) {
+    ctrl.empty()
+    for (const [k, v] of Object.entries(options)) {
+            ctrl.append($("<option />").val(k).text(v))
+    }
+}
+
 $( document ).ready( function() {
 
-    $("#ctrl_player").on("change", ctrl_player_OnChange)
-    // $("#baseMonth").on("change", saveValue)
-    // $("#timeClass").on("change", saveValue)
-    // $("#baseMatch").on("change", saveValue)
+    // on change dos controles
+    [USER_NAME, TIME_CLASS, BASE_MONTH, BASE_MATCH].forEach(id => $("#" + id).on("change", REFRESH_HTML_CONTROLS))
+
+    $("#btnClear").on("click", clearAll)
+    $("#loading-indicator").hide()
+    
+    
 
     // preenchendo os meses de referência
     curMonth = (new Date()).getMonth() + 1
-    curYear = (new Date()).getYear() + 1900
-    ctrl = $("#baseMonth")
+    curYear = (new Date()).getFullYear()
+    ctrl = $("#ctrl_base_month")
     
-    for (let i = 0; i < 12; i++) {
-        v = curYear + "/" + (curMonth < 10 ? "0" : "") + curMonth
+    options = {}
+    for (let i = 0; i < 18; i++) {
+        // base date, compatible with chess.com date format (epoch)
+        v = ((new Date(curYear + "-" + ((curMonth < 10 ? "0" : "") + curMonth) + "-01 00:00:00 GMT-0000")).getTime())/1000
         s = curMonth + "/" + curYear
-        ctrl.append($("<option />").val(v).text(s));
+        // ctrl.append($("<option />").val(v).text(s));
         curMonth -= 1
         if (curMonth == 0) {
             curMonth = 12
             curYear -= 1
         }
+        options[v] = s
     }
-    
 
+    setSelectOptions(ctrl, options)
     loadValues()
 })
 
@@ -282,77 +408,4 @@ function baseDate() {
     return new Date($('#baseMonth').val() + "/01")
 }
 
-/*
-*/
-games = null
 
-async function getInfo() {
-
-    // todos_jogos = "https://api.chess.com/pub/player/" + sUserName + "/games/archives"
-    // stats_url = "https://api.chess.com/pub/player/" + sUserName + "/stats"
-
-    // data atual
-    vBaseDate = [baseDate().getMonth() + 1, baseDate().getYear() + 1900]
-    vCurDate = [(new Date()).getMonth() + 1, (new Date()).getYear() + 1900]
-    currentUserName = currentUser()
-
-    // lista de datas a buscar, de base date até data atual no formato [mês, ano]
-    games_urls = []
-
-    while(true) {
-        games_urls.push("https://api.chess.com/pub/player/" + currentUserName + "/games/" + vBaseDate[1] + "/" + (vBaseDate[0] < 10 ? "0" : "") + vBaseDate[0])
-
-        if (JSON.stringify(vBaseDate) == JSON.stringify(vCurDate)) break
-
-        if (vBaseDate[0] < 12) {
-            vBaseDate[0] += 1
-        } else {
-            vBaseDate[0] = 1
-            vBaseDate[1] += 1
-        }
-    }
-
-    return (fetchURLs(games_urls))
-
-}
-
-async function updateOptions() {
-
-    const infos = await getInfo()
-    const allResults = []
-
-    for (data of infos) {
-
-        games = data.games.filter(function(e) {
-            return e.rated
-                // && e.time_control == "600"
-                // && e.time_class == time_class
-                && (e.end_time * 1000) >= baseDate().getTime()
-        })
-
-        games.forEach(e => {
-            v = {}
-            v.uuid = e.uuid
-            v.time_class = e.time_class
-            v.time_control = e.time_control
-            v.color = e.white["@id"].toLowerCase().indexOf(currentUser().toLowerCase()) > 0 ? "white" : "black"
-            v.opponent = e[v.color == "white" ? "black" : "white"]["@id"].split("/").pop()
-            v.rating = e[v.color].rating
-            result_str = e[v.color].result
-            v.result = result_str == "win" ? 1 : (result_str == "draw" ? 0.5 : 0)
-            
-            if (e.tournament && e.tournament.indexOf("/") >= 0) {
-                v.tournament = e.tournament.split("/").pop()
-            }
-
-            allResults.push(v)
-
-        });
-
-      
-        
-    }
-
-    return allResults    
- 
-}
